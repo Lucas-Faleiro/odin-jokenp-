10944934ebac8c5566a9c75e623edded906002d3
lucas-faleiro <16035833+Lucas-Faleiro@users.noreply.github.com>
2024-08-21 18:29:04 -0300

    Refactor playGame function, remove event listeners when game is over

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let computerScore = 0;
-let userScore = 0;
+let humanScore = 0;
 const buttonsList = document.querySelectorAll("button");
-const paraResult = document.querySelector("div");
+const resultDiv = document.querySelector("div");
+const scoreList = document.querySelectorAll(".score");
+const roundResult = document.querySelector("#roundResult");
+const computerChoicePara = document.querySelector("#computerChoice");
 
 function getRandomInt() {
   return Math.floor(Math.random() * 3);
@@ -10,66 +13,72 @@ function getRandomInt() {
 // returns random computer choice to pass as variable to playRound function
 function getComputerChoice() {
   let randomNumber = getRandomInt();
-  // console.log(randomNumber);
   switch (randomNumber) {
     case 0:
-      return "rock";
+      return "Rock";
     case 1:
-      return "paper";
+      return "Paper";
     case 2:
-      return "scissors";
+      return "Scissors";
   }
 }
 
-console.log(buttonsList);
+const playRound = (e) => {
+  const computerChoice = getComputerChoice();
+  console.log(`computer chooses: ${computerChoice}`);
+  computerChoicePara.innerText = `Computer Chooses: ${computerChoice}`;
 
-const playRound = buttonsList.forEach((button) => {
-  button.addEventListener("click", () => {
-    let computerChoice = getComputerChoice();
-    console.log(`computer chooses: ${computerChoice}`);
+  const humanChoice = e.target.innerText;
+  console.log(`human chooses: ${humanChoice}`);
 
-    const humanChoice = button.innerText.toLowerCase();
-    console.log(`human chooses: ${humanChoice}`);
+  if (humanChoice === computerChoice) {
+    roundResult.innerText = `It's a tie!`;
+  }
 
-    if (humanChoice === computerChoice) {
-      const roundResult = document.createElement("p");
-      roundResult.innerText = `It's a tie!`;
-      paraResult.appendChild(roundResult);
-    }
+  if (
+    (humanChoice === "Rock" && computerChoice === "Scissors") ||
+    (humanChoice === "Scissors" && computerChoice === "Paper") ||
+    (humanChoice === "Paper" && computerChoice === "Rock")
+  ) {
+    humanScore += 1;
+    scoreList[0].innerText = `Human Score: ${humanScore}`;
+    roundResult.innerText = `You Won! ${humanChoice} beats ${computerChoice}`;
+  }
 
-    if (
-      (humanChoice === "rock" && computerChoice === "scissors") ||
-      (humanChoice === "scissors" && computerChoice === "paper") ||
-      (humanChoice === "paper" && computerChoice === "rock")
-    ) {
-      userScore += 1;
-      const roundResult = document.createElement("p");
-      roundResult.innerText = `You Won! ${humanChoice} beats ${computerChoice}`;
-      paraResult.appendChild(roundResult);
-    }
+  if (
+    (humanChoice === "Rock" && computerChoice === "Paper") ||
+    (humanChoice === "Paper" && computerChoice === "Scissors") ||
+    (humanChoice === "Scissors" && computerChoice === "Rock")
+  ) {
+    computerScore += 1;
+    scoreList[1].innerText = `Computer Score: ${computerScore}`;
+    roundResult.innerText = `You Lost! ${computerChoice} beats ${humanChoice}`;
+  }
+};
 
-    if (
-      (humanChoice === "rock" && computerChoice === "paper") ||
-      (humanChoice === "paper" && computerChoice === "scissors") ||
-      (humanChoice === "scissors" && computerChoice === "rock")
-    ) {
-      computerScore += 1;
-      const roundResult = document.createElement("p");
-      roundResult.innerText = `You Lost! ${computerChoice} beats ${humanChoice}`;
-      paraResult.appendChild(roundResult);
-    }
-  });
+const playGame = () => {
+  if (humanScore === 3 || computerScore === 3) {
+    buttonsList.forEach((button) => {
+      button.removeEventListener("click", playRound);
+      button.removeEventListener("click", playGame);
+    });
+  }
+  if (humanScore === 3) {
+    const gameResult = document.createElement("p");
+    gameResult.innerText = "You won the game! Congratulations!";
+    resultDiv.appendChild(gameResult);
+  } else if (computerScore === 3) {
+    const gameResult = document.createElement("p");
+    gameResult.innerText = "The computer won the game! Too bad";
+    resultDiv.appendChild(gameResult);
+  }
+};
+
+buttonsList.forEach((button) => {
+  button.addEventListener("click", playRound);
 });
 
-// function playGame() {
-//   while (userScore != 3 || computerScore != 3) {
-//     playRound();
-//     if (userScore === 3) {
-//       return "You won the game! Congratulations!";
-//     } else if (computerScore === 3) {
-//       return "The computer won the game! Too bad";
-//     }
-//   }
-// }
-
-// console.log(playGame());
+//practicing for ... of
+for (button of buttonsList) {
+  button.addEventListener("click", playGame);
+}
